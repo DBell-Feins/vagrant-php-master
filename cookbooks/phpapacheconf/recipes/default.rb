3e5046506943e0b74dcd7aeacfe117f60525ca29
Dave Bell <bellfazar@gmail.com>
2013-03-29 13:38:23 -0400

    Added default /phpinfo url

--- cookbooks/phpapacheconf/recipes/default.rb
+++ cookbooks/phpapacheconf/recipes/default.rb
@@ -28,6 +28,24 @@ web_app "project" do
   set_env node['phpapacheconf']['set_env']
 end
 
+script "phpinfo" do
+  interpreter "bash"
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+  rm -rf /tmp/phpinfo*
+  mkdir -p /var/www/phpinfo
+  EOH
+  not_if "test -f /var/www/phpinfo"
+end
+
+template "/var/www/phpinfo/index.php" do
+  source "index.php.erb"
+  owner "root"
+  group "root"
+  mode 0755
+end
+
 service 'apache2' do
   action :restart
 end
